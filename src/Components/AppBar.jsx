4bfac6edaa9e837aa4b3df31c2505a25eed3fc29
MeenakshiPillai20 <pillai.meenakshi20@gmail.com>
2022-11-01 22:09:19 +0530

    first

---
+++ src/Components/AppBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AppBar = () => {
+    return (
+        <>
+           <div className="nav">
+               <a href="/" className="link"> Home</a>
+               <a href="/Student" className="link"> Student</a>
+               <a href="/Contact-us" className="link"> Contact-us</a>
+           </div>
+        </>
+    );
+}
+
+export default AppBar;
+
